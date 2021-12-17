5abb01e8bb93dc2ff11f088c8571154fa88be107
Colin Slater <ctslater@uw.edu>
2021-12-17 11:51:00 -0800

    Add argparse.

--- sync_new_logs.py
+++ sync_new_logs.py
@@ -6,32 +6,36 @@ from google.cloud import storage
 from itertools import chain
 import datetime
 import google.api_core.exceptions
-
-bucket_name = "drp-us-central1-logging"
-log_prefix = "Panda-RubinLog"
+import argparse
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Sync error logs from google")
+    parser.add_argument("bucket", type=str)
+    parser.add_argument("--prefix", type=str, nargs="?",
+                        help="Directory prefix inside the bucket")
+    parser.add_argument("-v", dest="verbose", action="store_true", help="verbose")
+
+    args = parser.parse_args()
 
     now = datetime.datetime.now(tz=datetime.timezone.utc)
     search_fmt_string =  "{base:s}/{year:d}/{month:02d}/{day:02d}/"
 
 
-    search_prefix = search_fmt_string.format(base=log_prefix,
+    search_prefix = search_fmt_string.format(base=args.prefix,
                                              year=now.year,
                                              month=now.month,
                                              day=now.day)
 
     yesterday = now - datetime.timedelta(days=1)
-    search_prefix_yesterday = search_fmt_string.format(base=log_prefix,
+    search_prefix_yesterday = search_fmt_string.format(base=args.prefix,
                                                        year=yesterday.year,
                                                        month=yesterday.month,
                                                        day=yesterday.day)
-    print(search_prefix, search_prefix_yesterday)
-
 
     storage_client = storage.Client.create_anonymous_client()
 
-    bucket = storage_client.bucket(bucket_name)
+    bucket = storage_client.bucket(args.bucket)
 
     files_to_download = chain(bucket.list_blobs(prefix=search_prefix),
                               bucket.list_blobs(prefix=search_prefix_yesterday))
@@ -39,6 +43,8 @@ if __name__ == '__main__':
     for bucket_file in files_to_download:
         etag_filename = "{:s}/.etag_{:s}".format(os.path.dirname(bucket_file.name),
                                                  os.path.basename(bucket_file.name))
+        if(args.verbose):
+            print(bucket_file.name)
 
         os.makedirs(os.path.dirname(bucket_file.name), exist_ok=True)
         try:
@@ -50,7 +56,8 @@ if __name__ == '__main__':
         try:
             bucket_file.download_to_filename(bucket_file.name, if_etag_not_match=etag_on_disk)
         except google.api_core.exceptions.NotModified:
-            print("already up-to-date, not downloading.")
+            if(args.verbose):
+                print("already up-to-date, not downloading.")
 
         with open(etag_filename, "w") as f:
             f.write(bucket_file.etag)
